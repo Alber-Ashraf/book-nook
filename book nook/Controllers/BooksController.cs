887c421b5293e45a8bb22063891448b4044fcd08
Alber Ashraf <beroooberooo6@gmail.com>
2023-02-14 15:34:08 +0200

    Adding Book (Modle , View , Controller)

---
+++ book nook/Controllers/BooksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using book_nook.Models;
+
+namespace book_nook.Controllers
+{
+    public class BooksController : Controller
+    {
+        public ActionResult Random()
+        {
+            var book = new Book() { Name = "doom" };
+            return View(book);
+        }
+    }
+}
---
+++ book nook/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace book_nook.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
