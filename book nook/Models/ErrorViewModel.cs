04a09e497a17f48aa324ddadd62b98859f42a1b0
Alber Ashraf <beroooberooo6@gmail.com>
2023-02-14 14:53:35 +0200

    Add project files.

---
+++ book nook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace book_nook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
